d6b9ef5129b38004bdb8624f2b20feb9994b81b0
John R. Estes <John.R.Estes@icloud.com>
2020-11-27 19:49:20 -0500

    updated store to reflect typeof

--- src/store/store.js
+++ src/store/store.js
@@ -11,6 +11,7 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined" ? a => a :
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
